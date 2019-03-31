ac2202ca50dff2d10dbb124cb5d396fd4c551c3e
Czkl <zw407461270@163.com>
2019-03-31 20:20:22 +0800

    first commit

---
+++ src/main/java/com/tw/simple/SpringBootGradleBaseApplication.java
@@ -0,0 +1,13 @@
+package com.tw.simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleBaseApplication.class, args);
+	}
+
+}
